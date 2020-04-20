13aa8f4204c67179795d68cd15fcc372c0083226
Arthur Limoge <30498203+ArthurLimoge@users.noreply.github.com>
2020-04-20 19:08:04 +0100

    Create Least_Squares.py
    This algorithm requires the function qr_reduction defined in Householder_QR_Reduction

---
+++ Least_Squares.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def least_squares(_input, target):
+    row_dim = _input.shape[0]
+    col_dim = _input.shape[1]
+    table = qr_reduction(_input)
+    _input, orthogonal, matrix = table[0], table[1], table[2]
+    rhs = np.matmul(np.transpose(orthogonal), target)  # right-hand side Q^t * b
+    sol_criterion = np.prod(rhs[col_dim:])  # exact solution iff b only has non-zero components in first col_dim rows
+    if sol_criterion == 0:
+        solution = np.matmul(np.linalg.inv(matrix), rhs[:col_dim])
+        return f"The system has an exact solution {solution}"
+    else:
+        rhs[col_dim:row_dim] = np.zeros(row_dim - col_dim)
+        solution = np.round(np.matmul(np.linalg.inv(matrix), rhs[:col_dim]), 3)
+        # norm = np.linalg.norm(target - np.matmul(_input, solution))  # residual norm
+        return solution
