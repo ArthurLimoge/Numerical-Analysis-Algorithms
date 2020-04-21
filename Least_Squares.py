2ed6cbccb6b5fce817a5fa7a3777ddd683138b15
Arthur Limoge <30498203+ArthurLimoge@users.noreply.github.com>
2020-04-21 08:22:07 +0100

    Update Least_Squares.py

--- Least_Squares.py
+++ Least_Squares.py
@@ -6,7 +6,7 @@ def least_squares(_input, target):
     table = qr_reduction(_input)  # if the matrix has significantly more rows, use parallel QR reduction instead
     _input, orthogonal, matrix = table[0], table[1], table[2]
     rhs = np.matmul(np.transpose(orthogonal), target)  # right-hand side Q^t * b
-    sol_criterion = np.prod(rhs[col_dim:])  # exact solution iff b only has non-zero components in first col_dim rows
+    sol_criterion = np.prod(rhs[col_dim:])  # exact solution iff b only has non-zero components in the first 'col_dim' rows
     if sol_criterion == 0:
         solution = np.matmul(np.linalg.inv(matrix), rhs[:col_dim])
         return f"The system has an exact solution {solution}"
