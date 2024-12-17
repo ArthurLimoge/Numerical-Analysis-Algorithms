a6763b4e33c8c757ef4dcc0fa76bfad16acdf2d4
Arthur Limoge <30498203+ArthurLimoge@users.noreply.github.com>
2024-12-18 00:06:39 +0100

    Update Least_Squares.py
    Added a call to parallel QR reduction

--- Least_Squares.py
+++ Least_Squares.py
@@ -1,13 +1,15 @@
 import numpy as np
+from Parallel_QR_Reduction import parallel_qr_reduction
 
 def least_squares(_input, target):
     _input = np.array(_input)
     row_dim = _input.shape[0]
     col_dim = _input.shape[1]
-    table = qr_reduction(_input)  # if the matrix has significantly more rows, use parallel QR reduction instead
+    table = qr_reduction(_input)  # if the matrix has significantly more rows, use parallel QR reduction instead (line below)
+#   table = parallel_qr_reduction(_input)
     _input, orthogonal, matrix = table[0], table[1], table[2]
     rhs = np.matmul(np.transpose(orthogonal), target)  # right-hand side Q^t * b
-    sol_criterion = np.prod(rhs[col_dim:])  # exact solution iff Q^t * b only has non-zero components in the first 'col_dim' rows
+    sol_criterion = np.prod(rhs[col_dim:])  # there exists an exact solution iff Q^t * b only has non-zero components in the first 'col_dim' rows
     if sol_criterion == 0:
         solution = np.matmul(np.linalg.inv(matrix), rhs[:col_dim])
         return f"The system has an exact solution {solution}"
